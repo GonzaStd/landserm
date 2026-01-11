43edbcce309848db70a9a8419f13780e6f4d7cf0
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-01-11 19:24:04 -0300

    First implementation of event objects. Now scan function only returns a list of events objects. It also reports missing services through events.

--- landserm/domains/services.py
+++ landserm/domains/services.py
@@ -1,9 +1,11 @@
 from landserm.config.system import getServicesData
+from landserm.core.events import Event
 
 def scan(services_config):
     services_data = getServicesData()
     target_services = list(services_config["include"])
     target_states = dict.fromkeys(target_services)
+    events = list()
     for line in services_data.splitlines():
         if not line.strip() or line.startswith("UNIT"):
             continue
@@ -12,6 +14,10 @@ def scan(services_config):
             state = line.split()[1]
             target_states[unit_name] = state
             target_services.remove(unit_name)
-
+            event = Event("services", "state", unit_name, state, "info")
+            events.append(event)
+    for missing in target_services:
+        event = Event("services", "state", missing, "missing", "info")
+        events.append(event)
     
-    return target_states, target_services
+    return events

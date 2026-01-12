d934fdc5249fee2449463dd55d27f3ea34586df2
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-01-11 21:46:16 -0300

    camelCase

--- landserm/semantics/types.py
+++ landserm/semantics/types.py
@@ -1,37 +1,37 @@
 from landserm.config.validators import *
 
-def parse_value(value: str, expected_type): 
+def parse_value(value: str, expectedType): 
     """ It returns valid values to store in yaml, and invalid values to display, 
     so the user can now what he did wrong.
     """
-    valid_values = []
-    invalid_values = []
-    inner_type = ""
+    validValues = []
+    invalidValues = []
+    innerType = ""
     # BOOL
     switch_bool = {
         "true": True,
         "false": False
     }
     
-    if (expected_type == "bool"):
+    if (expectedType == "bool"):
         value = value.lower()
         parsed = switch_bool.get(value, None)
         if (parsed != None):
-            valid_values.append(parsed)
+            validValues.append(parsed)
 
     # LIST
-    elif (expected_type.startswith("list")):
-        switch_list = {
+    elif (expectedType.startswith("list")):
+        switchList = {
         "partuuid": isPartuuid,
         "path": isPath,
         "service_name": isService
         }
-        inner_type = expected_type[5:-1]
+        innerType = expectedType[5:-1]
         values_list = value.split(",")
         for i in values_list:
-            if (switch_list.get(inner_type)(i)):
-                valid_values.append(i)
+            if (switchList.get(innerType)(i)):
+                validValues.append(i)
             else:
-                invalid_values.append(i)
+                invalidValues.append(i)
 
-    return valid_values, invalid_values, str(inner_type)
+    return validValues, invalidValues, str(innerType)

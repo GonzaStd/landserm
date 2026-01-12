2d7318ed371f136629cc869873290a4fac466acf
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-01-12 18:00:25 -0300

    There's a specifc scripts path to ensure there are scripts accesible only from there, so landserm user will have access to it, and user will have to use sudo to read, write and execute it. There will be a function for each action.  will call every action in the policy by mapping them with the supportedActions dictionary.

---
+++ landserm/core/actions.py
@@ -0,0 +1,44 @@
+import subprocess
+from landserm.config.loader import landsermRoot
+from landserm.config.validators import isPath
+
+scriptsPath = landsermRoot + "/config/scripts/"
+allowedVarsSet = {"domain", "kind", "subject", "payload"}
+
+
+
+def execScript(context: dict, scriptData: dict):
+    scriptName = str(scriptData.name)
+    if not scriptName.endswith(".sh"):
+        scriptName += ".sh"
+
+    scriptPath = scriptsPath + scriptName
+    if not isPath(scriptPath):
+        print("LOG: Invalid path or script", scriptName, "does not exist.")
+        return 1
+    
+    arguments = list(scriptData.args)
+    validArguments = list()
+
+    if arguments:
+        for arg in arguments:
+            arg = str(arg)
+            if arg.startswith("$") and arg[1:] in allowedVarsSet:
+                contextVarName = arg[1:]
+                if contextVarName in context.keys():
+                    validArguments.append(context[contextVarName]) 
+            else:
+                validArguments.append(arg)
+    
+    command = validArguments
+    command.insert(0, scriptPath)
+    subprocess.run(command, shell=False)
+
+supportedActions = {
+     "script": execScript
+}
+def executeActions(context: dict, allActions: dict):
+        for action in allActions:
+            actionData = allActions[action]
+            print("LOG: executing action", action)
+            supportedActions[action](context, actionData)

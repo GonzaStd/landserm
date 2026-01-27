0a0f79f9ef42d1121cafe9ae83c47ef99783bf6d
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-01-27 11:24:58 -0300

    I want the user to be able to personalize the policies as much as it can. That's why I changed payload to a dict.

--- landserm/observers/services.py
+++ landserm/observers/services.py
@@ -39,32 +39,45 @@ def checkStatus(servicesConfig):
     return events
 
 servicesConfig = loadConfig("services", domainsConfigPaths)
-last_state = dict().fromkeys(servicesConfig.get("include"))
-initialStates = checkStatus(servicesConfig)
+lastState = dict().fromkeys(servicesConfig.get("include"))
+initialStates = checkStatus(servicesConfig) # I need a function that throws events with payloads with more data. I'll implement "getServiceDetails"
 for event in initialStates:
-    last_state[event.subject] = event.payload
+    lastState[event.subject] = event.payload
 
 def handle_systemd_signal(msg):
     # msg has properties like path, interface, member, body, etc.
+    payload=dict()
     path = msg.path
     if not path.startswith("/org/freedesktop/systemd1/unit/"):
         return
     
     escaped_unit_name = path.split("/")[-1]
     unit_filename = unescape_unit_filename(escaped_unit_name)
+    unit_name = unit_filename[:-8]
 
     if len(msg.body) >= 2:
         interface = msg.body[0]
         changed = dict(msg.body[1])
         if interface == 'org.freedesktop.systemd1.Unit':
-            if "ActiveState" in changed:
-                unit_name = unit_filename[:-8]
-                state = changed.get("ActiveState").value # active/inactive/failed
-                if last_state.get(unit_name) == state:
-                    return
-                last_state[unit_name] = state
-                event = Event("services", "status", unit_name, state)
+            friendlyData = ["active", "sub", "load", "result", "exec_main", "pid"]
+            dbusData = {
+                "active": "ActiveState",
+                "sub": "SubState",
+                "load": "LoadState",
+                "result": "Result",
+                "exec_main": "ExecMainStatus",
+                "pid": "MainPID"
+            }
+            payload.fromkeys(friendlyData)
+            for fd in friendlyData:
+                if dbusData[fd] in changed:
+                    payload[fd] = changed.get(dbusData[fd]).value
+            unitLastState = lastState.get(unit_name)
+            if payload == unitLastState:
+                return
+            unitLastState[unit_name] = payload
+            event = Event(domain="services", kind="status", subject=unit_name, payload=payload)
 
-                policiesIndex, _ = policiesIndexation()
-                process([event], policiesIndex)
+            policiesIndex, _ = policiesIndexation()
+            process([event], policiesIndex)
 

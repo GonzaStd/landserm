e44d99903b52aa0a0073961f3aa0972e29b569db
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-01-28 19:33:59 -0300

    Changed names context and scriptdata, so it is more standard and understandable.
    Now context variables can expand through a dictionary (for example the payload one) as $payload.to.key . I also added  the "log" action delivery.

--- landserm/core/actions.py
+++ landserm/core/actions.py
@@ -1,14 +1,15 @@
 import subprocess
 from landserm.config.loader import landsermRoot
 from landserm.config.validators import isPath
+from landserm.core.delivery import deliveryLog
 
 scriptsPath = landsermRoot + "/config/scripts/"
 allowedVarsSet = {"domain", "kind", "subject", "payload"}
 
 
 
-def execScript(context: dict, scriptData: dict):
-    scriptName = str(scriptData["name"])
+def execScript(eventData: object, actionData: dict):
+    scriptName = str(actionData.get("name"))
     if not scriptName.endswith(".sh"):
         scriptName += ".sh"
 
@@ -17,28 +18,36 @@ def execScript(context: dict, scriptData: dict):
         print("LOG: Invalid path or script", scriptName, "does not exist.")
         return 1
     
-    arguments = list(scriptData.get("args")) if "args" in scriptData else False
+    arguments = list(actionData.get("args")) if "args" in actionData else False
     validArguments = list()
 
     if arguments:
         for arg in arguments:
             arg = str(arg)
-            if arg.startswith("$") and arg[1:] in allowedVarsSet:
-                contextVarName = arg[1:]
-                if contextVarName in context.keys():
-                    validArguments.append(context[contextVarName]) 
+            if arg.startswith("$"):
+                arg = arg[1:]
+                if arg in allowedVarsSet:
+                    validArguments.append(getattr(eventData, arg))
+                elif '.' in arg and arg.split(".")[0] == "payload":
+                    payloadData = dict(eventData.payload)
+                    keys = arg.split(".")[1:]
+                    pivot = payloadData
+                    for key in keys:
+                        pivot = pivot[key]
+                    validArguments.append(str(pivot))
             else:
-                validArguments.append(arg)
+                validArguments.append(str(arg))
     
     command = validArguments
     command.insert(0, scriptPath)
     subprocess.run(command, shell=False)
 
 supportedActions = {
-     "script": execScript
+     "script": execScript,
+     "log": deliveryLog
 }
-def executeActions(context: dict, allActions: dict):
+def executeActions(eventData: object, allActions: dict):
         for action in allActions:
             actionData = allActions[action]
             print("LOG: executing action", action)
-            supportedActions[action](context, actionData)
+            supportedActions[action](eventData, actionData)

ac6e293a52a785b3d1f1cbeeb91dcb5d7b417f7a
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-01-28 22:40:55 -0300

    Added timestamp, modified whole log message so it looks nicer. Added exception handling.

--- landserm/core/delivery.py
+++ landserm/core/delivery.py
@@ -1,8 +1,32 @@
+import json
+from datetime import datetime
 from landserm.config.loader import landsermRoot
+
 def deliveryLog(eventData: object, actionData: dict):
     if not actionData.get("enabled"):
         return 0
+    
     logPath = actionData.get("path", f"{landsermRoot}/logs/landserm-{eventData.domain}.log")
-    logMessage = f"\n---START---{eventData.kind} from {eventData.subject}:\n{eventData.payload}\n---END---"
-    with open(logPath, "a") as logFile:
-        logFile.write(logMessage)
+
+    if isinstance(eventData.payload, dict):
+        payload = json.dumps(eventData.payload, indent=2)
+    else:
+        payload = str(eventData.payload)
+    
+    timestamp = datetime.now().strftime("%B-%d-Y %H:%M:%S")
+
+    logMessage = f"""
+[{timestamp}] {eventData.domain.upper()} EVENT
+  Kind:     {eventData.kind}
+  Subject:  {eventData.subject}
+  Payload:
+{payload}
+{'='*50}
+"""
+    try:
+        with open(logPath, "a") as logFile:
+            logFile.write(logMessage)
+        print(f"LOG: Written to {logPath}")
+    except PermissionError:
+        print(f"ERROR: No permission to write to {logPath}. Change permissions or change to an allowed path.\
+               You can skip \"path\" in config and it will use the default path: {landsermRoot}/logs/landserm-{eventData.domain}.log")

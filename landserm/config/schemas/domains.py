dd30ef557842b1c56dbf964168160e1b2fc03d4e
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-02-08 22:16:31 -0300

    Now it validates after the creation of the instance.
    added selectDomain in order to work properly for v2

--- landserm/config/schemas/domains.py
+++ landserm/config/schemas/domains.py
@@ -1,14 +1,34 @@
-from pydantic import BaseModel, Field, field_validator
-from typing import Optional, List
+from pydantic import BaseModel, Field, model_validator
+from typing import Optional, List, Dict, Union
 
 class ServicesConfig(BaseModel):
     enabled: Optional[bool] = False
     include: Optional[List[str]] = None
 
-    @field_validator('include')
-    @classmethod
-    def validate_include(self, value, info):
-        enabled = info.data.get('enabled')
+    @model_validator(mode='after')
+    def validate_include(self):
+        if self.enabled and not self.include:
+            raise ValueError("You should config at least one service when services monitoring is enabled.")
+        return self
 
-        if enabled and not value:
-            raise ValueError("You should config at least one service when services monitoring is enabled (true).")
+domainsConfig = Union[ServicesConfig]
+
+def selectDomain(domain):
+    switch = {
+        "services": ServicesConfig
+        # In v2 there will be more domains.
+    }
+
+    domainClass = switch.get(domain)
+    if not domainClass:
+        raise ValueError(f"Unknown domain: {domain}")
+    
+    domainsConfig = type(
+        "DomainsConfig",
+        (BaseModel,),
+        {
+            "__annotations__": {"policies": Dict[str, domainClass]},
+            "policies": Field(default_factory=dict)
+        }
+    )
+    return domainsConfig

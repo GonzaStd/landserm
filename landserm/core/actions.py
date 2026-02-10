a90ddb3fb22e7b8177a5b41aedaaaf36649f0899
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-02-10 11:16:45 -0300

    Now you can specify a script path in the .env file.
    Path is handled correctly to avoid problems.
    
    Added a little bit of logging.
    
    I was returning a dict instead of objects because model_dump makes EVERYTHING a dict, including classess inside the class. Now it is fixed with model_fields.
    
    priority is a field but no an actionName so I skip it.
    
    All actions should know the priority, so now it is added as a parameter.

--- landserm/core/actions.py
+++ landserm/core/actions.py
@@ -1,22 +1,26 @@
 import subprocess
-from landserm.config.loader import landsermRoot
+from os import environ as env
 from landserm.config.validators import isPath
 from landserm.config.schemas.policies import ThenBase, ScriptAction
 from landserm.core.delivery import deliveryLog, deliveryOLED, deliveryPush
 from landserm.core.context import expand
 from landserm.core.events import Event
 
-scriptsPath = landsermRoot + "/config/scripts/"
 allowedVarsSet = {"domain", "kind", "subject", "systemdInfo", "payload"}
 
-
-
-def execScript(eventData: Event, scriptData: ScriptAction):
+def execScript(eventData: Event, scriptData: ScriptAction, policyActions: ThenBase):
+    scriptsPath = env.get("LANDSERM_SCRIPTS_PATH", "/etc/landserm/scripts/")
     scriptName = str(scriptData.name)
+    if scriptName.strip("/"):
+        scriptName = scriptName.strip("/")
+        
     if not scriptName.endswith(".sh"):
         scriptName += ".sh"
 
-    scriptPath = scriptsPath + scriptName
+
+    if scriptsPath.strip("/"):
+        scriptsPath = scriptsPath.strip("/")
+    scriptPath = f"/{scriptsPath}/{scriptName}"
     if not isPath(scriptPath):
         print("LOG: Invalid path or script", scriptName, "does not exist.")
         return 1
@@ -27,7 +31,9 @@ def execScript(eventData: Event, scriptData: ScriptAction):
         validArguments.append(expanded)
    
     command = [scriptPath] + validArguments
+    print(f"=== EXECUTING {scriptName} ===")
     subprocess.run(command, shell=False)
+    print(f"=== SCRIPT ENDED ===")
 
 supportedActions = {
      "script": execScript,
@@ -36,11 +42,19 @@ supportedActions = {
      "push": deliveryPush
 }
 def executeActions(eventData: Event, policyActions: ThenBase):
-    actionsDict = policyActions.model_dump(exclude_none=True)
-
-    for actionName, actionData in actionsDict.items():
+    priority = policyActions.priority
+    for actionName in type(policyActions).model_fields:
+        if actionName == "priority":
+            continue
+        
+        actionData = getattr(policyActions, actionName)
+        
+        if actionData is None:
+            continue
+        
         if actionName not in supportedActions:
-                print(f"WARNING: Unknown action '{actionName}'. Skipping.")
-                continue
-        print("LOG: executing action", actionName)
-        supportedActions[actionName](eventData, actionData)
+            print(f"WARNING: Unknown action '{actionName}'. Skipping.")
+            continue
+        
+        print(f"LOG: executing action {actionName}")
+        supportedActions[actionName](eventData, actionData, priority)

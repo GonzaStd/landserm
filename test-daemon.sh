baa882afb8ba866c0e156c73ade107d7b9a9790a
Gonzalo <gonzalo.vattino.secu@gmail.com>
2026-02-11 00:29:09 -0300

    Now it works, because I had to export PYTHONPATH so python detects modules correctly.

--- test-daemon.sh
+++ test-daemon.sh
@@ -1,2 +1,5 @@
-#/bin/bash
-bash -c "sudo -u landserm .venv/bin/python3 landserm/daemon/daemon.py"
+#!/bin/bash
+cd "$(dirname "$(readlink -f "$0")")"
+source .venv/bin/activate
+export PYTHONPATH="$(pwd)"
+python3 landserm/daemon/daemon.py
